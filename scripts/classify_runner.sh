d180b638f6873dd025ee8c946b16e0e89600c7bb
ryansingman <43079376+ryansingman@users.noreply.github.com>
2021-05-05 14:07:34 -0400

    Feature: classifier updates (#5)
    * added checkpoints dir to gitignore
    
    * added keras, tensorflow to setup config
    
    * created image classifier, fully functional with driver script
    
    * deleted minhs commit, added checkpointing
    
    * updated gitignore
    
    * working image classification, evaluation, results plotting
    
    * config edits, runner edits, plotter edits

--- scripts/classify_runner.sh
+++ scripts/classify_runner.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 printf "\n\n*** Running affine Classifier Training***\n\n"
-python scripts/classify.py configs/augmentation/affine.yaml configs/classifier/default.yaml --resume_epoch 30
-#python scripts/evaluate.py configs/augmentation/affine.yaml configs/classifier/default.yaml --load_epoch 30
+python scripts/classify.py configs/augmentation/affine.yaml configs/classifier/default.yaml --resume_epoch 50
+#python scripts/evaluate.py configs/augmentation/affine.yaml configs/classifier/default.yaml --load_epoch 50
 
 printf "\n\n*** Running all Classifier Training***\n\n"
-python scripts/classify.py configs/augmentation/all.yaml configs/classifier/default.yaml --resume_epoch 30
-#python scripts/evaluate.py configs/augmentation/all.yaml configs/classifier/default.yaml --load_epoch 30
+python scripts/classify.py configs/augmentation/all.yaml configs/classifier/default.yaml --resume_epoch 50
+#python scripts/evaluate.py configs/augmentation/all.yaml configs/classifier/default.yaml --load_epoch 50
 
 printf "\n\n*** Running frequency Classifier Training***\n\n"
-python scripts/classify.py configs/augmentation/frequency.yaml configs/classifier/default.yaml --resume_epoch 30
-#python scripts/evaluate.py configs/augmentation/frequency.yaml configs/classifier/default.yaml --load_epoch 30
+python scripts/classify.py configs/augmentation/frequency.yaml configs/classifier/default.yaml --resume_epoch 50
+#python scripts/evaluate.py configs/augmentation/frequency.yaml configs/classifier/default.yaml --load_epoch 50
 
 printf "\n\n*** Running intensity Classifier Training***\n\n"
-python scripts/classify.py configs/augmentation/intensity.yaml configs/classifier/default.yaml --resume_epoch 30
-#python scripts/evaluate.py configs/augmentation/intensity.yaml configs/classifier/default.yaml --load_epoch 30
+python scripts/classify.py configs/augmentation/intensity.yaml configs/classifier/default.yaml --resume_epoch 50
+#python scripts/evaluate.py configs/augmentation/intensity.yaml configs/classifier/default.yaml --load_epoch 50
 
 printf "\n\n*** Running original Classifier Training***\n\n"
-python scripts/classify.py configs/augmentation/original.yaml configs/classifier/default.yaml --resume_epoch 30
-#python scripts/evaluate.py configs/augmentation/original.yaml configs/classifier/default.yaml --load_epoch 30
-
+python scripts/classify.py configs/augmentation/original.yaml configs/classifier/default.yaml --resume_epoch 50
+#python scripts/evaluate.py configs/augmentation/original.yaml configs/classifier/default.yaml --load_epoch 50
